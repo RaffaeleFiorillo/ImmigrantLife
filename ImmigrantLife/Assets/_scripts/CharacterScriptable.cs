863af4fb4efe7e8ce4fb5094ea184e193e14e890
PRSemedo <paulosemedo.trabalho@gmail.com>
2024-11-26 17:04:08 +0000

    first draft
    acho que ja da para perceber e para mexeres

---
+++ ImmigrantLife/Assets/_scripts/CharacterScriptable.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterScriptable", menuName = "Scriptable Objects/CharacterScriptable")]
+public class CharacterScriptable : ScriptableObject
+{
+//char info
+    public string name;
+
+
+
+
+
+
+
+}
